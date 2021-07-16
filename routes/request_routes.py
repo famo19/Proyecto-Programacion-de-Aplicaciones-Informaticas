49a17a514b6e4388a3f8ef1b03485a67e4816b06
Elida Lapa cerros <elidalapa@gmail.com>
2021-07-16 14:41:34 -0600

    changes

--- routes/request_routes.py
+++ routes/request_routes.py
@@ -5,4 +5,20 @@ from logic.request_logic import RequestLogic
 class RequestRoutes:
     @staticmethod
     def configure_routes(app):
-        @app.route("/request")
+        @app.route("/request", methods=["GET", "POST"])
+        def createReq():
+            if session["userType"] == "cliente":
+                ReqLogic = RequestLogic
+                if request.method == "GET":
+                    allRequests = ReqLogic()
+                    return render_template("request.html")
+                elif request.method == "POST":
+                    logic = RequestLogic()
+                    selectedName = request.form["name"]
+                    selectedEmail = request.form["email"]
+                    selectedBook = request.form["book"]
+                    selectedYear = request.form["year"]
+                    selectedAuthor = request.form["author"]
+                    selectedMessage = request.form["message"]
+                    rows = logic.insertRequest(selectedName,selectedEmail,selectedBook,selectedYear,selectedAuthor,selectedMessage)
+                    return render_template("request.html")
