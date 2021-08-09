949fd54ca063b673637828de8d8c919521b6104a
Verónica Ferman <veronica.fermanperez@gmail.com>
2021-08-09 10:32:29 -0600

    card confirmation and api functionality

--- routes/libreria_routes.py
+++ routes/libreria_routes.py
@@ -30,8 +30,22 @@ class LibreriaRoutes:
             categoria = logicCate.getCatById(idCat)
             if session["userAccount"] == "gratis":
                 return render_template("booksFree.html", result=result, categoria=categoria)
-            else:
-                return render_template("booksPaid.html", result=result, categoria=categoria)
+            elif session["userAccount"] == "premium":
+                #link para pdf y audio
+                urlpdf = f"https://esen-restapi.herokuapp.com/pdf/{titulo}"
+                urlaudio = f"https://esen-restapi.herokuapp.com/audio/{titulo}"
+                responsepdf = requests.get(urlpdf)
+                responseaudio = requests.get(urlaudio)
+                if responsepdf.status_code == 200 & responseaudio.status_code == 200:
+                    dataJsonPdf = responsepdf.json()
+                    linkpdf = dataJsonPdf["archivo"]
+
+                    dataJsonAudio = responseaudio.json()
+                    linkaudio = dataJsonAudio["archivo"]
+
+                    return render_template("booksPaid.html", result=result, categoria=categoria, linkpdf=linkpdf, linkaudio=linkaudio)
+                else:
+                    return render_template("booksPaid.html", result=result, categoria=categoria)
 
         @app.route("/libreria/books/addedBook")
         def addBook():
@@ -64,3 +78,16 @@ class LibreriaRoutes:
             logicBook = LibreriaLogic()
             rows = logicBook.deleteBookByTitle(titulo, userId)
             return redirect("/libreria")
+
+        """API ROUTES"""
+        @app.route("/libreria/books/pdf")
+        def pdfBook():
+            #sacar datos del current viewing book
+            titulo = session["viewingBook"]
+            url = f"https://esen-restapi.herokuapp.com/audio/{titulo}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                dataJson = response.json()
+                return render_template("places.html", id=id, place=dataJson)
+            else:
+                return redirect("/dashboard")
